1f934d5f4c4da7725ae5de64c4ea3a892ba6b520
Sören Krings <soeren.krings@jimdo.com>
2017-11-26 15:25:42 +0100

    More cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,23 @@
+process.env.DEBUG = 'actions-on-google:*';
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const Assistant = require('actions-on-google').ApiAiAssistant;
+
 admin.initializeApp(functions.config().firebase);
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  console.log('headers: ' + JSON.stringify(request.headers));
+  console.log('body: ' + JSON.stringify(request.body));
 
-  firebase.database().ref('messages').push({
+  const assistant = new Assistant({request: request, response: response});
+
+  admin.database().ref('messages').push({
     name: 'Some name',
     text: 'I am chilling!'
+  }).then(() => {
+    // send json back
+    // dialogflow sdk
+    response.send("Good chilling!");
   });
-
-  // response
-  response.send("Hello from Firebase!");
 });
